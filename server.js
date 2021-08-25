f528353ae7313c21208f4b50e556d9aabc832092
Oscar Flores <85149090+oscarif04@users.noreply.github.com>
2021-08-24 21:54:54 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -10,38 +10,27 @@ const app = express();
 //middleware
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
-app.use(express.static("./develop/public"));
+app.use(express.static("public"));
 
 
 //creating API routes, saving notes, and joins w/ db.json
-app.get("/api/notes", function(req,res) => {
+app.get("/api/notes", function(req,res) {
     res.sendFile(path.join(__dirname, "/db/db.json"))
 });
 
 
 //post func to enter new notes
-app.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync(".develop/db/db.json"));
+app.post("/api/notes", function(req, res) {
+    const notes = JSON.parse(fs.readFileSync("/db/db.json"));
     const newNotes = req.body;
-    newNotes.id = uuid.v4();
     notes.push(newNotes);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes))
     res.json(notes)
 });
 
 
-//deleting notes
-app.delete("/api/notes/:id", (req, res) => {
-    const notes = JSON.parse(fs/fs.readFileSync("./db/db.json"));
-    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
-    fs.writeFileSync("./db/db,json", JSON.stringify(delNote));
-    res.json(delNote);
-});
-
-
-
 //calling HTML Starting Page
-app.get("/*", function(req, res) {
+app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
